cdf1ce66c2d8271893572e870d603f63ded5bf45
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-11-07 00:10:45 +0300

    Completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_06_071328) do
+ActiveRecord::Schema.define(version: 2022_11_06_114928) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
